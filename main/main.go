79127312ea86ac3843b65bc0d2b831ff0fab8dcd
LukasJarolim <lukasjarolim01@seznam.cz>
2025-08-11 11:03:03 +0200

    Removed main error

--- main/main.go
+++ main/main.go
@@ -88,16 +88,6 @@ func main() {
 			br := webtools.NewUDPBridge("127.0.0.1:7777", "127.0.0.1:17777")
 			br.Start()
 		}
-	case "utb":
-		{
-			sv, _ := webtools.NewUDPToTCPBridge("127.0.0.1:7777", "127.0.0.1:5679", true)
-			sv.Start()
-		}
-	case "tub":
-		{
-			cl, _ := webtools.NewTCPToUDPBridge("127.0.0.1:5681", "127.0.0.1:17777", true)
-			cl.Start()
-		}
 	//case "hpsu":
 	//	{
 	//		sv := webtools.NewHTTPProxyServerUDP("127.0.0.1:5679", "127.0.0.1:7777", true)
