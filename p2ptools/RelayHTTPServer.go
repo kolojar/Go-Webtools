5775329349f4c18a32389f1a5f8395327c8cfb90
LukasJarolim <lukasjarolim01@seznam.cz>
2025-09-09 06:09:01 +0200

    Working on P2P - Currently pausing development

--- p2ptools/RelayHTTPServer.go
+++ p2ptools/RelayHTTPServer.go
@@ -39,9 +39,81 @@ func PackRelayMessage(isCommand bool, data []byte) []byte {
 }
 
 /*
-Unpacks Relay ,
+Unpacks Relay
 */
 func UnpackRelayMessage(message []byte) (isCommand bool, data []byte) {
 	isCommandByte := message[0]
 	return string(isCommandByte) == "1", message[1:]
+}
+
+func (sv *P2PHTTPCoordinatorServer) readFuncRelay(conn *httptools.WebSocketServerConn, data []byte, status uint8, isBinary bool) {
+	if sv.relaysByClient.Get(conn) == nil {
+		//No connection found, create new
+		sv.relaysByClient.Set(conn, &RelayServerConnection{conn: conn})
+	}
+	sv.readFuncRelayLocal(sv.relaysByClient.Get(conn), data, status, isBinary)
+}
+
+/*
+Function for handling relay requests
+*/
+func (sv *P2PHTTPCoordinatorServer) readFuncRelayLocal(conn *RelayServerConnection, data []byte, status uint8, isBinary bool) {
+	//No relay allowed
+	if !sv.allowRelay {
+		sv.httpServer.Logger.Log(3, "No relay allowed.")
+		conn.Close()
+		return
+	}
+
+	//Sort status
+	if status == webtools.TCP_CONNECT_STATUS {
+		//On connect
+		conn.conn.IsBinary = true
+		conn.SendCommand(RELAY_CMD_GET_RELAY_INFO, nil)
+	}
+
+	if !isBinary {
+		//Command
+		command, params, hadError := UnpackP2PMessage(sv.asymmetricEncryption, sv.httpServer.Logger, conn..publicKey, data)
+		if hadError {
+			return
+		}
+
+		//Sort error command
+		if params["error"] != "" {
+			//Got error from client
+			sv.httpServer.Logger.Log(3, "Client returned error: "+params["error"])
+			conn.Close()
+			return
+		}
+
+		//Sort commands
+		switch command {
+		case RELAY_CMD_GET_RELAY_INFO:
+			{
+				if params["peerId"] == "" {
+					//No peer id
+					sv.httpServer.Logger.Log(3, "No peerId property specified.")
+					conn.Close()
+					return
+				}
+
+				//Get peer connection
+				peerConn := sv.peersById.Get(params["peerId"])
+				if peerConn == nil {
+					//No peer found
+					sv.httpServer.Logger.Log(3, "Invalid peerId specified.")
+					conn.Close()
+					return
+				}
+
+				//Associate peer to relay connections
+				peerConn.relayConn = conn
+			}
+
+		}
+
+	} else {
+		//Data
+	}
 }
