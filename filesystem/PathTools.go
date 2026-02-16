15ce7a92bbe560bae2acfba8afb1e3d96f34bcd9
LukasJarolim <lukasjarolim01@seznam.cz>
2026-02-16 08:38:21 +0100

    Fix

--- filesystem/PathTools.go
+++ filesystem/PathTools.go
@@ -1,34 +1,10 @@
 package filesystem
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 )
-
-var iNVALID_NAMES = [...]string{"..", "."}
-
-/*
-Checks if path constains some invalid names (server protection) -> Returns TRUE if value is INVALID
-! Must be present in every operation with files on server !
-*/
-func CheckInvalidNames(path string) error {
-	if !strings.HasPrefix(path, "/") {
-		fmt.Println("Path does not start with /")
-		return os.ErrInvalid
-	}
-	split := strings.Split(path, "/")
-	for i := 0; i < len(iNVALID_NAMES); i++ {
-		for k := 0; k < len(split); k++ {
-			if strings.EqualFold(split[k], iNVALID_NAMES[i]) {
-				fmt.Println("Found invalid name: " + iNVALID_NAMES[i] + " in: " + path)
-				return os.ErrInvalid
-			}
-		}
-	}
-	return nil
-}
 
 /*
 JoinPathSecure joins path and checks if it hasnt escaped from basePath. Returns error os.ErrInvalid if path escaped. PAth must exist (only last part does not have to)
