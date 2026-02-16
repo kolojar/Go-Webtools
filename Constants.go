f30a2d9b5fd9f7d031bdc5fc2df81945ff26b229
LukasJarolim <lukasjarolim01@seznam.cz>
2026-02-16 10:13:12 +0100

    Added none

--- Constants.go
+++ Constants.go
@@ -8,21 +8,26 @@ const BufferSize = 1024 * 16
 type NetworkStatus uint8
 
 /*
+ConnectStatus is status of connect
+*/
+const NoneNetworkStatus NetworkStatus = 0
+
+/*
 ReadDataStatus is status of read
 */
-const ReadDataStatus NetworkStatus = 3
+const ReadDataStatus NetworkStatus = 4
 
 /*
 ConnectStatus is status of connect
 */
-const ConnectStatus NetworkStatus = 0
+const ConnectStatus NetworkStatus = 1
 
 /*
 DisconnectStatus is status of disconnect
 */
-const DisconnectStatus NetworkStatus = 1
+const DisconnectStatus NetworkStatus = 2
 
 /*
 FinishedReadFuncStatus is status of finished reading of one read function and switching to other
 */
-const FinishedReadFuncStatus NetworkStatus = 2
+const FinishedReadFuncStatus NetworkStatus = 3
