671a66cc17258c6c2b2bfa318973b461b5de7efb
LukasJarolim <lukasjarolim01@seznam.cz>
2026-03-19 19:11:56 +0100

    Handshake defragmentation

--- webrtc/DTLSProcessor.go
+++ webrtc/DTLSProcessor.go
@@ -7,11 +7,11 @@ import (
 )
 
 type DTLSProcessor struct {
-	buildedFragmentedRequests webtools.SafeMap[uint64, DTLSRecord]
-	currentEpoch              uint16
-	replayWindowPrevious      ReplayWindow[uint64]
-	replayWindowCurrent       ReplayWindow[uint64]
-	logger                    *webtools.ConsoleLogger
+	fragmentedHandshakeBuilders webtools.SafeMap[uint16, DTLSRecord]
+	currentEpoch                uint16
+	replayWindowPrevious        ReplayWindow[uint64]
+	replayWindowCurrent         ReplayWindow[uint64]
+	logger                      *webtools.ConsoleLogger
 }
 
 func (processor *DTLSProcessor) ProcessData(reader io.Reader) (completeRecords []DTLSRecord, hasNonDTLSData bool, err error) {
@@ -26,23 +26,24 @@ func (processor *DTLSProcessor) ProcessData(reader io.Reader) (completeRecords [
 		return nil, true, err
 	}
 
+	completeRecords = make([]DTLSRecord, 0)
 	for _, record := range records {
 		//Check if Epoch is valid
 		processor.logger.Log(0, "Processing DTLS record: type="+strconv.FormatUint(uint64(record.ContentType), 10)+"; epoch="+strconv.FormatUint(uint64(record.Epoch), 10)+"; sequenceNumber="+strconv.FormatUint(record.SequenceNumber, 10))
 		if !(processor.currentEpoch == record.Epoch || processor.currentEpoch-1 == record.Epoch) {
-			processor.logger.Log(1, "Dropping record with epoch: "+strconv.FormatUint(uint64(record.Epoch), 10))
+			processor.logger.Log(2, "Dropping record with epoch: "+strconv.FormatUint(uint64(record.Epoch), 10))
 			continue
 		}
 
 		//Check if packet was already recieved
 		if processor.currentEpoch == record.Epoch {
 			if !processor.replayWindowCurrent.ApplyWindowCheck(record.SequenceNumber) {
-				processor.logger.Log(1, "Dropping record with sequence number: "+strconv.FormatUint(record.SequenceNumber, 10))
+				processor.logger.Log(2, "Dropping record with sequence number: "+strconv.FormatUint(record.SequenceNumber, 10))
 				continue
 			}
 		} else if processor.currentEpoch-1 == record.Epoch {
 			if !processor.replayWindowPrevious.ApplyWindowCheck(record.SequenceNumber) {
-				processor.logger.Log(1, "Dropping record with sequence number: "+strconv.FormatUint(record.SequenceNumber, 10))
+				processor.logger.Log(2, "Dropping record with sequence number: "+strconv.FormatUint(record.SequenceNumber, 10))
 				continue
 			}
 		}
@@ -50,7 +51,34 @@ func (processor *DTLSProcessor) ProcessData(reader io.Reader) (completeRecords [
 		//Valid packet
 		if record.ContentType == DTLSRecordContentTypeHandshake {
 			//Handshake
-			record.Fragment.(DTLSHandshakeFragment)
+			fragment := record.Fragment.(DTLSHandshakeFragment)
+			var fragmentProcessor DTLSHandshakeFragmentProcessor
+			if processor.fragmentedHandshakeBuilders.Has(fragment.MessageSequence) {
+				//Get processor
+				fragmentProcessor = processor.fragmentedHandshakeBuilders.Get(fragment.MessageSequence).Fragment.(DTLSHandshakeFragmentProcessor)
+			} else {
+				//Create new processor
+				fragmentProcessor = MakeDTLSHandshakeFragmentProcessor(int(fragment.Length))
+			}
+
+			//Process
+			isCompleteHandshake, handshake := fragmentProcessor.Process(fragment)
+			if isCompleteHandshake {
+				//Completed
+				processor.fragmentedHandshakeBuilders.Delete(fragment.MessageSequence)
+				record := processor.fragmentedHandshakeBuilders.Get(fragment.MessageSequence)
+				record.Fragment = handshake
+				processor.logger.Log(1, "Completed record with message sequence number: "+strconv.FormatUint(uint64(fragment.MessageSequence), 10))
+				completeRecords = append(completeRecords, record)
+				continue
+			}
+
+			//Not complete
+			record.Fragment = fragmentProcessor
+			processor.logger.Log(0, "Fragmenting record with message sequence number: "+strconv.FormatUint(uint64(fragment.MessageSequence), 10))
+			processor.fragmentedHandshakeBuilders.Set(fragment.MessageSequence, record)
+			continue
 		}
 	}
+	return completeRecords, false, nil
 }
